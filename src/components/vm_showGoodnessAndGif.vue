b1725af33b119a89d6907495dc1f8943fe3c69ac
Wang1ing <2745758440@qq.com>
2022-10-26 14:47:01 +0800

    10.26.14.30.提交.添加产品优势的介绍所用的vue界面添加v-for，（tips：v-for不能用在根结点上）

--- src/components/vm_showGoodnessAndGif.vue
+++ src/components/vm_showGoodnessAndGif.vue
@@ -1,13 +1,15 @@
 <template>
-  <div class="the_main_div">
-    <img v-if="show_gif_status" :src="png_gif_list[index].png_url" alt="zwsj"
-         @mouseover="changegif()" @mouseout="changegif()">
-    <img v-else :src="png_gif_list[index].gif_url" alt="zwsj"
-         @mouseover="changegif()" @mouseout="changegif()">
-    <div>
-      极速直播
-    </div>
-    <div class="bottom_line">
+  <div>
+    <div class="the_main_div" v-for="(item , index) in png_gif_list" :key="index">
+      <img v-if="item.show_gif_status" :src="item.png_url" alt="zwsj"
+           @mouseover="item.show_gif_status = !item.show_gif_status" @mouseout="item.show_gif_status = !item.show_gif_status">
+      <img v-else :src="item.gif_url" alt="zwsj"
+           @mouseover="item.show_gif_status = !item.show_gif_status" @mouseout="item.show_gif_status = !item.show_gif_status">
+      <div>
+        极速直播
+      </div>
+      <div class="bottom_line">
+      </div>
     </div>
   </div>
 </template>
@@ -17,18 +19,16 @@ export default {
   name: 'vm_showGoodnessAndGif',
   data () {
     return {
-      show_gif_status: true,
-      index: 0,
-      png_gif_list: [{png_url: require('../assets/png_or_gif/01.png'), gif_url: require('../assets/png_or_gif/01.gif')},
-        {png_url: require('../assets/png_or_gif/02.png'), gif_url: require('../assets/png_or_gif/02.gif')},
-        {png_url: require('../assets/png_or_gif/03.png'), gif_url: require('../assets/png_or_gif/03.gif')},
-        {png_url: require('../assets/png_or_gif/04.png'), gif_url: require('../assets/png_or_gif/04.gif')},
-        {png_url: require('../assets/png_or_gif/05.png'), gif_url: require('../assets/png_or_gif/05.gif')},
-        {png_url: require('../assets/png_or_gif/06.png'), gif_url: require('../assets/png_or_gif/06.gif')},
-        {png_url: require('../assets/png_or_gif/07.png'), gif_url: require('../assets/png_or_gif/07.gif')},
-        {png_url: require('../assets/png_or_gif/08.png'), gif_url: require('../assets/png_or_gif/08.gif')},
-        {png_url: require('../assets/png_or_gif/09.png'), gif_url: require('../assets/png_or_gif/09.gif')},
-        {png_url: require('../assets/png_or_gif/10.png'), gif_url: require('../assets/png_or_gif/10.gif')}
+      png_gif_list: [{png_url: require('../assets/png_or_gif/01.png'), gif_url: require('../assets/png_or_gif/01.gif'), show_gif_status: true},
+        {png_url: require('../assets/png_or_gif/02.png'), gif_url: require('../assets/png_or_gif/02.gif'), show_gif_status: true},
+        {png_url: require('../assets/png_or_gif/03.png'), gif_url: require('../assets/png_or_gif/03.gif'), show_gif_status: true},
+        {png_url: require('../assets/png_or_gif/04.png'), gif_url: require('../assets/png_or_gif/04.gif'), show_gif_status: true},
+        {png_url: require('../assets/png_or_gif/05.png'), gif_url: require('../assets/png_or_gif/05.gif'), show_gif_status: true},
+        {png_url: require('../assets/png_or_gif/06.png'), gif_url: require('../assets/png_or_gif/06.gif'), show_gif_status: true},
+        {png_url: require('../assets/png_or_gif/07.png'), gif_url: require('../assets/png_or_gif/07.gif'), show_gif_status: true},
+        {png_url: require('../assets/png_or_gif/08.png'), gif_url: require('../assets/png_or_gif/08.gif'), show_gif_status: true},
+        {png_url: require('../assets/png_or_gif/09.png'), gif_url: require('../assets/png_or_gif/09.gif'), show_gif_status: true},
+        {png_url: require('../assets/png_or_gif/10.png'), gif_url: require('../assets/png_or_gif/10.gif'), show_gif_status: true}
       ]
     }
   },
